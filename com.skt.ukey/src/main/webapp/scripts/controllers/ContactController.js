cecbb8e9b8d809180784d6e724bead6557cd78b4
han-seungil <dev.seungil.han@gmail.com>
2014-06-23 20:35:19 +0900

    Ready

---
+++ com.skt.ukey/src/main/webapp/scripts/controllers/ContactController.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+(function(){
+	var app = angular.module('ukey-contact',[]);
+	app.controller('ContactController',['$http',function($http){
+		var ukey=this;
+		ukey.info=[];
+		this.search = function(){
+			$http.get('/ukey/contactInfo.json').success(function(data){
+				ukey.info = data;
+				console.log(data);
+			});
+		};
+	}]);
+})();
---
+++ com.skt.ukey/src/main/webapp/scripts/controllers/CustomerController.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+(function(){
+	var app = angular.module('ukey-customer',[]);
+	app.controller('CustomerController',[ '$http', function($http){
+		this.message='this is customer controller';
+		var ukey=this;
+		ukey.info=[];
+		
+		
+		this.search = function(){
+			$http.get('/ukey/customer.json').success(function(data){
+				ukey.info = data;
+			});
+		};
+		
+		//Test function
+		this.clickTest=function(){
+			alert(this.info.contactMain1st);
+		};
+	}]);
+})();
